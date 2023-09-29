9f08d83a7a6e92b620b32ce0a98a53c5dedcfc5c
atrefilov <alexx308@mail.ru>
2023-09-29 08:31:42 +0400

    Done

---
+++ src/Praktikum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            // здесь нужно вывести результат
+            System.out.println("12.09." + year);
+        } else {
+            System.out.println("13.09." + year);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        // здесь нужно определить, является ли переданный год високосным
+        return year % 4 == 0 && (year % 400 == 0 || year % 100 != 0);
+    }
+}
